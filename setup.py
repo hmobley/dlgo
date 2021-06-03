954070b131886ff40300e99187b7fbee439fa719
Rich Devine <hankdman@hotmail.com>
2021-06-03 02:52:32 -0400

    updated version as that seems to be the only way to install bug fixes

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='dlgo',
-      version='0.4',
+      version='0.5',
       description='Deep Learning and the Game of Go',
       url='http://github.com/hmobley/dlgo',
       install_requires=[
