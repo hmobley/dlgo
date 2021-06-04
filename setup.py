be994205d34e8247e7da1f87f6fe74de159b73d3
Rich Devine <hankdman@hotmail.com>
2021-06-03 22:57:31 -0400

    removed some print statements

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='dlgo',
-      version='0.24',
+      version='0.25',
       description='Deep Learning and the Game of Go',
       url='http://github.com/hmobley/dlgo',
       install_requires=[
